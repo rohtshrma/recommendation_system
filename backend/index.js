b6fbb9ea034dd27bd24ddb41e21e2beed2ccd2cc
Arbaz Alam <drarbazalam@gmail.com>
2022-01-23 12:22:51 +0530

    Added MongoAtlas

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors=require('cors');
-var app = express();
-const port = process.env.PORT || 5000;
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
+const CONNECTION_URL =
+  "mongodb+srv://arbaz:arbaz9305@cluster0.ozthe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 8000;
 
-
-app.get("/",(req,res)=>{
-
-    res.json("hello");
-})
- 
-
-app.listen(port, function () {
-  console.log("Welcome you to recommendation system");
-});
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log("Server Running!!")))
+  .catch((err) => console.log(err));
