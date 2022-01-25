1f31891ab2ab4146fc554abe17812f3130ae320f
Abrar Ahmad <abrardim112@gmail.com>
2022-01-25 13:16:42 +0530

    carousel working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import Home from "./components/Home/Home";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import OwlCarousel from 'react-owl-carousel';
+import'owl.carousel/dist/assets/owl.carousel.min.css';
+import'owl.carousel/dist/assets/owl.theme.default.css';
 function App() {
   return (
     <div className="App">
       <Home />
+      
     </div>
   );
 }
