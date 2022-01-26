4219827bb21a40928ae5e1d776c95235d3617a61
rohtshrma <rohitsharma22000rs@gmail.com>
2022-01-26 20:52:54 +0530

    photos changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import "./Header.css";
-import logoImg from "../images/demo_icon.jpg";
+import logoImg from "../images/FastRivi-logos.jpeg";
+import bannerImg from "../images/main-banner-4.png"
 
 function Header() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          companyName
-        </a>
+      
 
         <div id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
-                <img src={logoImg} className="company_logo" alt="Logo" />
+              <a className="nav-link" style={{backgroundColor: "#f8f9fa"}} href="#">
+                <img src={logoImg} className="company_logo"  alt="Logo" />
               </a>
             </li>
           </ul>
         </div>
+        <a className="navbar-brand" href="#">
+          FastRivi
+        </a>
       </nav>
 
       {/* header end */}
@@ -35,13 +37,14 @@ function Header() {
         <div className="container">
           <div className="row justify-content-center ">
             <div className="col-md-12 align-self-center text-center">
+            <img src={bannerImg}  className="bannerImage" alt="bannerImg"/>
               {/* <span className="badge rounded-pill badge-inverse text-white font-weight-light px-3 py-1">Creating Brands</span> */}
-              <h1 className=" title text-white " style={{ fontSize: "35px" }}>
+              <h1 className=" title text-white " style={{ fontSize: "35px", fontFamily:" 'Sedgwick Ave', cursive" }}>
                 Get Product recommendation according to your Need
               </h1>
               <h6
                 className="text-white font-weight-normal op-8"
-                style={{ fontSize: "35px" }}
+                style={{ fontSize: "35px", fontFamily:" 'Sedgwick Ave', cursive" }}
               >
                 What are you planning to buy?
               </h6>
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
   return (
     <>
       <Header />
+      {/* <br/> */}
       <Carousel />
     </>
   );
