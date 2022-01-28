21d618b3960abc888676722ecb30ec144e4cbe51
Abrar Ahmad <abrardim112@gmail.com>
2022-01-29 02:12:22 +0530

    carousel and slide menu is updated

--- frontend/src/components/Questions/Sidebar.jsx
+++ frontend/src/components/Questions/Sidebar.jsx
@@ -1,29 +1,39 @@
 import React from "react";
 import { Stack } from "react-bootstrap";
 import "./style.css";
+import Paper from '@mui/material/Paper';
+
+// import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <Stack gap={3} className="mainContainer">
-        <div>
-          <button className="active">Get Started</button>
-        </div>
-        <div>
-          <button>Children </button>
-        </div>
+    <>
 
-        <div>
+    <div className="">
+      <Stack gap={3} className="mainContainer">
+      <Paper className="question_slide">
+        <div className="slide_box">
+          <button className="active">Get Started </button>
+        </div>
+        <div className="slide_box">
+          <button>Children   </button>
+          
+        </div>
+        
+
+        <div className="slide_box">
           <button>Room Size</button>
         </div>
-        <div>
+        <div className="slide_box">
           <button>Price</button>
         </div>
-        <div>
+        <div className="slide_box">
           <button>Results</button>
         </div>
+        </Paper>
       </Stack>
     </div>
+    </>
   );
 }
 
