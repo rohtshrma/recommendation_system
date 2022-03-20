2518afff4c76b2711d3fd879d4dc98e823e7ade7
Arbaz Alam <drarbazalam@gmail.com>
2022-03-21 00:07:32 +0530

    Changes added

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ mongoose.connect(CONNECTION_URL, {
 // Making Build Folder as Public
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
